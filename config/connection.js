1585f413cddbe9eb63f783bbc671655003e6e17a
Kaviz101586 <jason.mapou@gmail.com>
2019-03-18 20:05:17 -0400

    minor fix

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,18 @@ var mysql = require("mysql");
 
 var connection;
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
- mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
 }
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
